fb48f84b578ca4d462c7567be98a7cde92bdc469
P3ntest <xXraxFraeyXx@gmail.com>
2020-11-12 10:39:37 +0100

    update test fpr modules.ts

--- src/test/moduleLoaderTest.js
+++ src/test/moduleLoaderTest.js
@@ -1,4 +1,6 @@
 const ModuleLib = require("../libs/modules");
 const path = require("path");
 
-let manager = new ModuleLib.ModuleManager([path.join(__dirname, "../modules"), path.join(__dirname, "../defaultModules")]);
+let manager = new ModuleLib.ModuleManager();
+manager.load(path.join(__dirname, "../modules"));
+manager.load(path.join(__dirname, "../defaultModules"));
