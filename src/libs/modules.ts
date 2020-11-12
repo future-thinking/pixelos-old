8a1ff8765a050e89e4ff95025d26f8f1526d80fb
P3ntest <xXraxFraeyXx@gmail.com>
2020-11-12 08:53:22 +0100

    fully migrate modules.ts

--- src/libs/modules.ts
+++ src/libs/modules.ts
@@ -1,8 +1,16 @@
 import fs from "fs";
 import path from "path";
 import chalk from "chalk";
+import { config } from "process";
+import { SrvRecord } from "dns";
 
 export class Module {
+    path: string;
+    jsonConfig: any;
+    constructor(path: string, jsonConfig: any) {
+        this.path = path;
+        this.jsonConfig = jsonConfig;
+    }
 }
 
 export class ModuleManager {
@@ -12,18 +20,20 @@ export class ModuleManager {
         this.modules = [];
 
         pathsToModules.forEach(pathToModules => {
-            fs.readdirSync(pathToModules).forEach((file) => {
-                if (fs.lstatSync(path.join(pathToModules, file)).isDirectory()) {
-                    if (fs.existsSync(path.join(pathToModules, file, "package.json"))) {
-                        let module = new Module();
-                        this.modules.push(module);
-                    } else {
-                        console.log(chalk.red("Module " + chalk.blue(file) + 
-                        " has no " + chalk.underline("package.json") + ". It will not be loaded."));
+            if (fs.existsSync(pathToModules)) {
+                fs.readdirSync(pathToModules).forEach((file) => {
+                    if (fs.lstatSync(path.join(pathToModules, file)).isDirectory()) {
+                        if (fs.existsSync(path.join(pathToModules, file, "module.json"))) {
+                            let jsonConfig = require(path.join(pathToModules, file, "module.json"));
+                            let module = new Module(path.join(pathToModules, file), jsonConfig);
+                            this.modules.push(module);
+                        } else {
+                            console.log(chalk.red("Module " + chalk.blue(file) + 
+                            " has no " + chalk.underline("module.json") + ". It will not be loaded."));
+                        }
                     }
-                }
-            });
+                });
+            }
         });
-        
     }
 }
--- src/test/moduleLoaderTest.js
+++ src/test/moduleLoaderTest.js
@@ -1,4 +1,4 @@
-const ModuleLib = require("../dist/src/modules");
+const ModuleLib = require("../libs/modules");
 const path = require("path");
 
 let manager = new ModuleLib.ModuleManager([path.join(__dirname, "../modules"), path.join(__dirname, "../defaultModules")]);
