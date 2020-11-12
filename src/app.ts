4d4d6aabf6bb9ffd36f65bb2a010522d66a8df4c
P3ntest <xXraxFraeyXx@gmail.com>
2020-11-12 10:38:25 +0100

    update modulesManager; add init and load to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,11 @@
 import {TableScreen, Color, Frame} from "./libs/screen";
 import config from "./config.json";
+import { ModuleManager } from "./libs/modules";
+import path from "path";
 
 var mainScreen = new TableScreen(config.emulator.onlyEmulate, config.screen);
+mainScreen.init();
+
+var moduleManager = new ModuleManager();
+moduleManager.load(path.join(__dirname, "defaultModules"));
+moduleManager.load(path.join(__dirname, "modules"));
--- src/libs/modules.ts
+++ src/libs/modules.ts
@@ -5,35 +5,46 @@ import { config } from "process";
 import { SrvRecord } from "dns";
 
 export class Module {
-    path: string;
-    jsonConfig: any;
-    constructor(path: string, jsonConfig: any) {
-        this.path = path;
-        this.jsonConfig = jsonConfig;
-    }
+  path: string;
+  jsonConfig: any;
+  constructor(path: string, jsonConfig: any) {
+    this.path = path;
+    this.jsonConfig = jsonConfig;
+  }
 }
 
 export class ModuleManager {
-    modules: Array<Module>;
+  modules: Array<Module>;
 
-    constructor(pathsToModules: Array<string>) {
-        this.modules = [];
+  constructor() {
+    this.modules = [];
+  }
 
-        pathsToModules.forEach(pathToModules => {
-            if (fs.existsSync(pathToModules)) {
-                fs.readdirSync(pathToModules).forEach((file) => {
-                    if (fs.lstatSync(path.join(pathToModules, file)).isDirectory()) {
-                        if (fs.existsSync(path.join(pathToModules, file, "module.json"))) {
-                            let jsonConfig = require(path.join(pathToModules, file, "module.json"));
-                            let module = new Module(path.join(pathToModules, file), jsonConfig);
-                            this.modules.push(module);
-                        } else {
-                            console.log(chalk.red("Module " + chalk.blue(file) + 
-                            " has no " + chalk.underline("module.json") + ". It will not be loaded."));
-                        }
-                    }
-                });
-            }
-        });
+  load(pathToModules: string): void {
+    if (fs.existsSync(pathToModules)) {
+      fs.readdirSync(pathToModules).forEach((file) => {
+        if (fs.lstatSync(path.join(pathToModules, file)).isDirectory()) {
+          if (fs.existsSync(path.join(pathToModules, file, "module.json"))) {
+            let jsonConfig = require(path.join(
+              pathToModules,
+              file,
+              "module.json"
+            ));
+            let module = new Module(path.join(pathToModules, file), jsonConfig);
+            this.modules.push(module);
+          } else {
+            console.log(
+              chalk.red(
+                "Module " +
+                  chalk.blue(file) +
+                  " has no " +
+                  chalk.underline("module.json") +
+                  ". It will not be loaded."
+              )
+            );
+          }
+        }
+      });
     }
+  }
 }
